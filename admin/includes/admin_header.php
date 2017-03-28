fb6ba4954da277ffe6cdb60c1a806ecee1d7ca7c
Wasid Hossain <wasidmail@gmail.com>
2017-03-28 18:21:11 +0000

    implemented partial permission using

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,7 +1,16 @@
 <?php include "../includes/db.php"?>
 <?php include "function.php"?>
 <?php ob_start(); ?>
-<?php session_start(); ?>
+<?php session_start(); 
+
+if (!isset($_SESSION['user_role'])) {
+    header("Location: ../index.php");
+}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
