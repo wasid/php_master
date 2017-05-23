2d04516df60968fd4fba1589dd612cc1ae58ea37
Wasid Hossain <wasidmail@gmail.com>
2017-05-23 10:55:56 +0000

    Added LOADER to the CMS Admin

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -36,6 +36,7 @@ elseif (isset($_SESSION['user_role'])) {
 
     <!-- Custom CSS -->
     <link href="css/sb-admin.css" rel="stylesheet">
+    <link href="css/sb-admin.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
@@ -46,6 +47,10 @@ elseif (isset($_SESSION['user_role'])) {
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+    
+    <!--Custom CSS-->
+    <link href="css/style.css" rel="stylesheet">
+    
     <!-- Google Chart -->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <!--TinyMCE-->
